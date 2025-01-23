70a6010d2d13e611a92a90a1f4341ce5a853f714
nikushabitskinashvili <130066334+nikushabitskinashvili@users.noreply.github.com>
2025-01-24 00:52:57 +0400

    feat: crate first todo app in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgFor } from '@angular/common';
+
+export interface TodoItem {
+  id: number;
+  task: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, FormsModule, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'angular-todo';
+  todoList: TodoItem[] = [];
+  newTask: string = '';
+  addTask(): void {
+    if (this.newTask.trim() !== '') {
+      const newTodoItem: TodoItem = {
+        id: Date.now(),
+        task: this.newTask,
+      };
+
+      this.todoList.push(newTodoItem);
+      this.newTask = '';
+    }
+  }
+
+  deleteTask(id: number): void {
+    this.todoList = this.todoList.filter((item) => item.id !== id);
+  }
 }
